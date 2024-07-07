3b0c640b904bc2d862751b57e3b5cece3d7ff764
yodakazu <yodystudying@gmail.com>
2024-07-07 15:25:20 +0900

    add Dockerfile

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
-    from .routes import main_bp
-    app.register_blueprint(main_bp)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .routes import test_bp, users_bp
+    app.register_blueprint(test_bp)
+    app.register_blueprint(users_bp)
 
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
+from .models import db, User
 
-main_bp = Blueprint('main', __name__)
+test_bp = Blueprint('test', __name__)
 
-@main_bp.route('/api/hello', methods=['GET'])
+@test_bp.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!!")
+
+users_bp = Blueprint('users', __name__, url_prefix='/users')
+
+@users_bp.route('/', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = User(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}), 201
