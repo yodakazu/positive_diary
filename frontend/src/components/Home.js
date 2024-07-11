35630e94aeddaf55c3ad0e379da51e1c825d724e
yodakazu <yodystudying@gmail.com>
2024-07-11 15:56:58 +0900

    add home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">Welcome to Home Page</h1>
+      <p className="paragraph">This is a simple React component template.</p>
+    </div>
+  );
+};
+
+export default Home;
